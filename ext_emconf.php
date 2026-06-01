542e51814e33b153ea0058e9a407d8f7413454b3
pliou <45556500+pliou@users.noreply.github.com>
2026-06-01 23:36:09 +0200

    Prepare 13.4.0 release

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,11 +7,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Pawel Pliousnin',
     'author_email' => 'pliousnin@ppl-ds.com',
     'state' => 'stable',
-    'version' => '12.4.1',
+    'version' => '13.4.0',
     'clearCacheOnLoad' => 0,
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
